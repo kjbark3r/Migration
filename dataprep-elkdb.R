b4d240ae8da97d75b6763421ca2d97e8b4f52fe2
MCWRU01-LW17\kristin <kristin.barker@umconnect.umt.edu>
2017-10-09 15:54:12 -0600

    Determine, format, and export: a) all cow locations, b) all capture data, c) populations and years of interest, d) individual locations of interest, and e) capture data for individuals of interest. Remove all NA locations and Ski Hill elk from Sapphire population.

--- dataprep-elkdb.R
+++ dataprep-elkdb.R
@@ -2,8 +2,10 @@
 #           FORMATTING STATEWIDE ELK GPS DATA             #
 #    TO ASSESS FACTORS INFLUENCING MIGRATORY BEHAVIOR     #
 #                   KRISTIN BARKER                        #
-#                   SEPTEMBER 2017                        #
+#                    OCTOBER 2017                         #
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+
+
 
 
 ### ### ### ### ###
@@ -12,196 +14,168 @@
 
 
 
-
 #### packages ####
 
-library(RODBC) #possibly unnecessary; for connecting to access
-library(sp) #for kernel centroid estimate
-library(adehabitatHR) #for kernel centroid estimate
-library(rgdal) #for latlong/stateplane conversions
-library(gsubfn)
-library(maptools) #for writeSpatialShape
-library(dplyr) #for joins
-
+  library(RODBC) # for connecting to access
+  library(dplyr) # for joins and general awesomeness
 
 
 
 #### working directories and database connection ####
 
-wd_workcomp <- "C:\\Users\\kristin.barker\\Documents\\GitHub\\Migration"
-wd_laptop <- "C:\\Users\\kjbark3r\\Documents\\GitHub\\Migration"
-wd_worklaptop <- "C:\\Users\\kristin\\Documents\\Migration"
-if (file.exists(wd_workcomp)) {setwd(wd_workcomp)
-} else {
-  if(file.exists(wd_laptop)) {setwd(wd_laptop)
+  wd_workcomp <- "C:\\Users\\kristin.barker\\Documents\\GitHub\\Migration"
+  wd_laptop <- "C:\\Users\\kjbark3r\\Documents\\GitHub\\Migration"
+  wd_worklaptop <- "C:\\Users\\kristin\\Documents\\Migration"
+  if (file.exists(wd_workcomp)) {setwd(wd_workcomp)
   } else {
-    setwd(wd_worklaptop)
+    if(file.exists(wd_laptop)) {setwd(wd_laptop)
+    } else {
+      setwd(wd_worklaptop)
+    }
   }
-}
-if(file.exists(wd_worklaptop)) {
- channel <- odbcDriverConnect("Driver={Microsoft Access Driver (*.mdb, *.accdb)};
-                              dbq=C:/Users/kristin/Documents/DatabasesEtc/Statewide_Elk_GPS.accdb")
-} else {  cat("Maybe you shouldn't have been so lazy when you made this code") }
-rm(wd_workcomp, wd_laptop, wd_worklaptop)
+  if(file.exists(wd_worklaptop)) {
+   channel <- odbcDriverConnect("Driver={Microsoft Access Driver (*.mdb, *.accdb)};
+                                dbq=C:/Users/kristin/Documents/DatabasesEtc/Statewide_Elk_GPS.accdb")
+  } else {  cat("Maybe you shouldn't have been so lazy when you made this code") }
+  rm(wd_workcomp, wd_laptop, wd_worklaptop)
 
-
-
-#### RAW DATA FROM ACCESS DATABASE ####
-
-collardays <- sqlQuery(channel, paste("select * from GPS_Herd_TimePeriod"))
-rawcap <- sqlQuery(channel, paste("select * from CaptureInfo_allelk"))
-rawlocs <- sqlQuery(channel, paste("select * from ElkGPS"))
-
-
-#### DATA PER POPULATION ####
-
-
-# herds that have at least 1 and 2 yrs of collar data ####
-
-    n365 <- filter(collardays, CollarDays >= 365)
-    length(unique(n365$Herd)) # 19 herds with at least 1 year of data for at least 1 elk
-    
-    n730 <- filter(collardays, CollarDays >= 730)
-    length(unique(n730$Herd)) # 10 herds with at least 2 yrs of data for at least 1 elk
-    # since moving to 2 yrs of data cuts number of herds nearly in half, prob will only use 1 yr
-
-    
-# indivduals in each herd with 1 or 2 years of data ####
-    
-    i365 <- n365 %>%
-      group_by(Herd) %>%
-      count(nIndiv = n_distinct(AnimalID)) %>%
-      ungroup() 
-    # range 1 - 48 (11 have >= 10)
-
-    i730 <- n730 %>%
-      group_by(Herd) %>%
-      count(nIndiv = n_distinct(AnimalID)) %>%
-      ungroup()     
-    # yeeeeah, definitely not getting more than 1 year per herd
-    
-    
-# herds with at least 1 yr of data (to filter from larger db) ####
-    
-    herds <- data.frame(Herd = unique(n365$Herd))
-    
-    
-# check whether we're losing herds with collardays in the 300s (but below 365)
-      
-    hm <- collardays %>%
-      filter(CollarDays >299 & CollarDays <= 365) %>%
-      anti_join(hm, n365, by = "Herd")
-    # excellent, not losing any herds in the 300s 
-    # making 315 the generic cutoff because that's the fewest days between
-        # a capture event and the subsequent winter
-        # (so all indivs should have a chance to return to winter range)
-    
-
-# populations and years of interest ####
-    
-    popnyrs <- collardays %>%
-      filter(CollarDays >= 315) %>%
-      group_by(Herd) %>%
-      summarise(FirstDate = min(MinOfDate),
-                LastDate = max(MaxOfDate),
-                nIndiv = n_distinct(AnimalID)) %>%
-      ungroup() %>%
-      filter(nIndiv >= 10) %>%
-      mutate(Yr1 = as.numeric(substr(FirstDate, 0, 4)),
-             Yr2 = Yr1+1,
-             YrEnd = as.numeric(substr(LastDate, 0, 4))) %>%
-      arrange(nIndiv) %>%
-      filter(YrEnd > 2006) # no NDVI data prior to 2005
-    write.csv(popnyrs, file = "popns-yrs.csv", row.names = F)
-
-    herds <- data.frame(Herd = popnyrs$Herd)
-    write.csv(herds, file = "herds.csv", row.names = F)
-    
-    sum(yrs$nIndiv)
-    nrow(yrs)
-    # 457 individuals across 15 populations :) (but incls males etc)
 
   
-# summary capture information per population of interest ####
+  
+### ### ### ### ### ###
+####  |DATA PREP|  ####
+### ### ### ### ### ###
+  
 
-    # capture dates per popn (to define start date of analysis for indivs)
-    capdates <- rawcap %>%
-      filter(!is.na(CaptureDate)) %>%
-      semi_join(herds, by = "Herd") %>%
-      mutate(CapYr = substr(CaptureDate, 0, 4)) %>%
-      group_by(Herd, CapYr) %>%
-      summarise(LastCapDate = max(CaptureDate)) %>%
-      ungroup()
-    
-    # capture dates only for indivs of interest in popns of interest
-    capindivs <- rawcap %>%
-      filter(!is.na(CaptureDate)) %>%
-      semi_join(herds, by = "Herd") %>%
-      filter(Sex == "F") %>%
-      select(AnimalID, CollarID, Herd, CaptureArea, CaptureDate, Age, Age_Type, GPSdata)
-    write.csv(capindivs, file = "prelim-capindivs.csv", row.names=F)
-      
-    
-#### DATA PER INDIV ####
-    
-    #### ** in progress ** ####
-    
-    # read in data created above (if skipping from raw data to here #
-    
-    popnyrs <- read.csv("popns-yrs.csv")
-    herds <- read.csv("herds.csv")
-    capindivs <- read.csv("prelim-capindivs.csv")
+  
+#### raw data from access database ####
 
-    # only females in popns of interest ####
-    allindivs <- rawlocs %>%
-      semi_join(herds, by = "Herd") %>% # only popns of interest
-      filter(Sex == "F") %>% # only females
-      select(-c(Herd, CollarID)) %>% # avoid duplicate columns
-      right_join(capindivs, by = "AnimalID") %>% # add capture info
-      mutate(CapYr = substr(CaptureDate, 0, 4)) %>%
-      filter(Date <= paste0(CapYr, "-12-31")) %>%
-      mutate(Month = substr(Date, 6, 7)) 
+  rawcap <- sqlQuery(channel, paste("select * from CaptureInfo_allelk"))
+  rawlocs <- sqlQuery(channel, paste("select * from ElkGPS"))
+  
 
-    #### YOU NEED TO SOMEHOW
-      # use the INFO above to pull only locs from yr1 for each indiv
-      # but group_by will fuck it up because you'll lose all your locations
-      # so figure out a way to remove individuals that have locations in dec during capture year
-      # without losing their actual locations from the df above
+  
+#### alterations/fixes prior to actual summaries and analyses ####
 
-      
-    
-    # number of locations per day per indiv ####
+  
+  # remove indivs that should not be considered in popn HR estimation
+  # ("ski hill" elk from sapphires and bulls from any population)
+  rmelk <- filter(rawcap, CaptureArea == "Ski Hill") # this also removes NAs
+  allcowlocs <- rawlocs %>%
+    filter(Sex == "F" & !is.na(Latitude)) %>%
+    anti_join(rmelk, by = "AnimalID")
+  write.csv(allcowlocs, file = "locs-allcows.csv", row.names = F)  
+  
+  
+  # remove trailing whitespace from Madison herd name;
+  # identify South Pioneers and West Pioneers as Pioneers;
+  # remove "ski hill" elk
+  fixedcap <- rawcap %>%
+    mutate(Herd = trimws(Herd)) %>%
+    mutate(Herd = ifelse(grepl("Pioneer", Herd), "Pioneers", Herd)) %>%
+    filter(AnimalID != "") %>%
+    anti_join(rmelk, by = "AnimalID")
+  write.csv(fixedcap, file = "capdat-allindivs.csv", row.names = F)
+  
+  
 
-      
-      
-      
-#### CHECKING THINGS OUT ####
-
-# testcode: pulling month from posix date [in dplyr pipe]
-dats <- sample(indivs$Date, 10)
-mos <- dats$month
-
-# any december captures? (to define year1 properly)
-  unique(as.Date(collardays$MinOfDate)) # whoa, yep, even november.
-    
-    
-    
-    
-    
-        
-# OLD CODE ####    
-alllocs <- rawlocs %>%
-  # format date
-  within(Date <- as.Date(Date, "%Y-%m-%d")) %>%
-  # add month, year, season, and HRYear (match Dec from previous calendar year with subsequent Jan and Feb)
-  mutate(Month = months(Date, abbreviate = TRUE),
-         Year = format(as.Date(Date, format = "%Y-%m-%d"), "%Y"),
-         Season = ifelse(Month == "Dec" | Month == "Jan" | Month == "Feb", "Winter", 
-                         ifelse(Month == "Mar" | Month == "Apr" | Month == "May", "Spring",
-                                ifelse(Month == "Jun" | Month == "Jul" | Month == "Aug", "Summer",
-                                       "Fall"))),
-         HRYear = ifelse(Month == "Dec", as.numeric(Year)+1, as.numeric(Year)))
+  
+### ### ### ### ### ###
+####  |DATA WORK|  ####
+### ### ### ### ### ###
 
 
+  
+####  Identify populations and years of interest  ####
+  
+  popnyrs <- rawlocs %>%
+    # remove bulls, NA locations, and ski hill elk
+    filter(Sex =="F" & !is.na(Latitude)) %>% 
+    anti_join(rmelk, by = "AnimalID") %>%
+    # format and create date/time/year/month columns
+    within(Date <- as.Date(Date)) %>%
+    within(Time <- strftime(Time, format="%H:%M:%S")) %>%
+    mutate(Year = substr(Date, 0, 4), 
+           Month = as.numeric(substr(Date, 6, 7))) %>%
+    # only incl indivs with locs spanning entire yr (thru dec)
+    group_by(AnimalID, Year) %>%
+    # month>8 accts for e/w fork captures starting in nov
+    filter(max(Month == 12) & dplyr::n_distinct(Month > 8)) %>%
+    ungroup() %>%
+    # count number indivs per herd per yr
+    group_by(Herd, Year) %>%
+    summarise(nIndiv = n_distinct(AnimalID)) %>%
+    ungroup() %>%
+    group_by(Herd) %>%
+    # identify yr with most indivs
+    filter(nIndiv == max(nIndiv),
+           nIndiv > 2, # 2 indivs per grp not enough
+           Year > 2004) %>% # no ndvi data prior to 2005
+    ungroup() 
+  write.csv(popnyrs, file = "popns-yrs.csv", row.names = F)
+  sum(popnyrs$nIndiv) # print total indivs
+    
 
+  
+####  Identify individuals and locations of interest ####
+
+  
+  # determine last date of capture for each popn
+  capdates <- fixedcap %>%
+    filter(!is.na(CaptureDate)) %>%
+    mutate(Year = substr(CaptureDate, 0, 4)) %>%
+    # only for popns and years of interest
+    semi_join(popnyrs, by = c("Herd", "Year")) %>%
+    # determine month of capture
+    mutate(CaptureMonth = as.numeric(substr(CaptureDate, 6, 7))) %>%
+    # determine last date of capture for each popn
+    group_by(Herd) %>%
+    # account for e/w fork captures starting in nov for subsequent yr
+    filter(CaptureMonth < 10) %>%
+    summarise(LastCapDate = max(CaptureDate)) %>%
+    ungroup()
+    
+
+  
+  # only keep locs not collected during capture of that popn
+  locs <- rawlocs %>%
+    filter(Sex == "F" & !is.na(Latitude)) %>%
+    anti_join(rmelk, by = "AnimalID") %>%
+    # format and create date/time/year/month columns
+    within(Date <- as.Date(Date)) %>%
+    within(Time <- strftime(Time, format="%H:%M:%S")) %>%
+    mutate(Year = substr(Date, 0, 4),
+           Month = as.numeric(substr(Date, 6, 7))) %>%
+    # only use locs from popns and yrs of interest
+    semi_join(popnyrs, by = c("Herd", "Year")) %>%
+    # add capture date info
+    left_join(capdates, by = "Herd") %>%
+    # remov locs collected during capture
+    mutate(LastCapDate = as.Date(LastCapDate)) %>%
+    filter(Date > LastCapDate) %>%
+    # remove indivs who didn't make it through complete yr
+    group_by(AnimalID) %>%
+    mutate(MaxMonth = max(Month)) %>%
+    ungroup() %>%
+    filter(MaxMonth > 11)
+  write.csv(locs, file = "locs.csv", row.names = F)
+
+  # sanity check- verify capture and collar data id'd same #indivs
+  length(unique(locs$AnimalID)); sum(popnyrs$nIndiv)  # yes, n=356
+  
+
+  # filter capture data to only include individuals of interest
+  # and only keep original capture data for those who were recaptured
+  indivs <- data.frame(AnimalID = unique(locs$AnimalID))
+  allcap <- fixedcap %>%
+    semi_join(indivs, by = "AnimalID") %>%
+    arrange(AnimalID, CaptureDate)
+  cap <- allcap[!duplicated(allcap$AnimalID),]
+  write.csv(cap, file = "capdat.csv", row.names = F)
+  
+
+# POSSIBLE ADDITIONAL TASKS TO INCLUDE HERE
+  
+  # create dataframe of indiv, ageDURINGYEAR OF INTEREST, popn, year?
+  
 
