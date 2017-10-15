d41364e36441df8b199ae35447dc724f5051de42
MCWRU01-LW17\kristin <kristin.barker@umconnect.umt.edu>
2017-10-15 16:14:07 -0600

    so much failure, so much learning

--- zTroubleshooting/ztroubleshooting-locs.R
+++ zTroubleshooting/ztroubleshooting-locs.R
@@ -410,3 +410,454 @@ test <- indivlocs %>%
   group_by(AnimalID) %>%
   summarise(count = n())
 
+
+
+
+#### creating one polygon covering all popns of interest
+
+## create buffered area covering all population ranges
+test <- unionSpatialPolygons(popnhrsall, popnhrsall@data$id)
+length(popnhrsall@polygons)
+par(mfrow=c(1,2))
+plot(popnhrsall); plot(test)
+# i think this just unions disparate polygons per each herd
+# not all polygons together
+# could i change the underlying data of the spdf to id all as same herd tho?
+  # i don't think so; just changing data doesn't change length of polygons slot
+    # which is exactly what you're trying to change, incidentally...
+
+library(rgeos)
+test <- gUnion(popnhrsall, byid = FALSE)
+# newp, this requires 2 separate objects to union
+
+require(sp)
+test <- aggregate(popnhrsall, FUN = sum, dissolve = TRUE)
+popnhrsall
+
+plot(popnhrsall); plot(test)
+par(mfrow=c(1,1))
+plot(popnhrsall); plot(test, add=T)
+
+## try kde instead of mcp for full popn area
+# bc mcp incls a bunch of unoccupied space bt herds
+# and kde may buffer better anyway
+
+kde <- kernelUD(spdf.sp, grid = 500)
+kdevert <- getverticeshr(kde, percent = 100)
+# can't estimate this at 100% bc grid is too small
+# says rerun w lgr extent but tried 1 and 100, no diff
+# trying diff grid sizes instead
+plot(kdevert)
+par(mfrow=c(1,1))
+plot(kdevert, col = "blue"); plot(test, add=T)
+# nope, this loses a bunch of satellite type locations for 
+
+## estimate mcp for all populations
+popnhrsallmcp <- mcp(spdf.sp, percent = 100) #,3 = Herd
+
+
+## ok lets try that again
+# grid size 500 is unreasonably large, but 100 was a bit too small
+
+## mcps for all populations
+popnhrsallmcp <- mcp(spdf.sp, percent = 100) #,3 = Herd
+popnhrsmcp <- mcp(spdf.sp[,3], percent = 100) #,3 = Herd
+
+## trying diff grid sizes
+popnhrsallkde <- kernelUD(spdf.sp, grid = 50)
+popnhrsoutline <- getverticeshr(popnhrsallkde, percent = 100)
+par(mfrow=c(1,1))
+plot(popnhrsoutline, col = "red", main = "Grid50"); plot(popnhrsmcp, add = T, col = "blue")
+
+
+popnhrsallkde <- kernelUD(spdf.sp, grid = 100)
+popnhrsoutline <- getverticeshr(popnhrsallkde, percent = 100)
+par(mfrow=c(1,1))
+plot(popnhrsoutline, col = "red", main = "Grid100"); plot(popnhrsmcp, add = T, col = "blue")
+
+
+popnhrsallkde <- kernelUD(spdf.sp, grid = 110)
+popnhrsoutline <- getverticeshr(popnhrsallkde, percent = 100)
+par(mfrow=c(1,1))
+plot(popnhrsoutline, col = "red", main = "Grid110"); plot(popnhrsmcp, add = T, col = "blue")
+plot(spdf.sp, add = T)
+
+#closest so far?
+popnhrsallkde <- kernelUD(spdf.sp, grid = 115)
+popnhrsoutline <- getverticeshr(popnhrsallkde, percent = 100)
+par(mfrow=c(1,1))
+plot(popnhrsoutline, col = "red", main = "Grid115")#; plot(popnhrsmcp, add = T, col = "blue")
+plot(spdf.sp, add = T)
+# ok, looks like 110 is our cutoff grid size. need to buffer it a bit to incl those few weird locs.
+
+
+
+### buffer 1km (jic, and to cover those few outlying locs)
+popnbuff <- buffer(popnhrsoutline, dist = 1000)
+#x is missing - need spatial pixels df
+data("lynxjura")
+str(lynxjura)
+str(lynxjura$map)
+# maybe just create a sppix from the sppoly?
+test <- as(popnhrsoutline, "SpatialPixelsDataFrame")
+# can't do that, gotta make a new spatial pixels df
+pix <- SpatialPixelsDataFrame(xy, popnlocsall, proj4string = latlong)
+# error, dimension 1: coordinate intervals are not constant
+spix <- as(popnhrsoutline, "SpatialPixels")
+# no method for coercing... make it from scratch
+  # NEED: SpatialPoints coordinates, proj4string, maybe grid?)
+str(xy) #df, need spatialpoints
+pts <- spTransform(SpatialPoints(xy, proj4string = latlong), stateplane)
+# cool, looks like that might have actually workes
+pix <- SpatialPixels(pts, proj4string = stateplane)
+# still withthe coordinate intervals
+# it's trying to make a grid from the points rather than underlying data
+
+library(rgeos)
+wtf2 <- gBuffer(popnhrsoutline, width = 1)
+plot(wtf2, col = "blue", main = "width1") 
+plot(wtf, add = T, col ="red") #wtf was the original "buffer" that i think wasn't
+# ok width1 is the same, must be default, not buffered.
+
+test <- gBuffer(popnhrsoutline, width = 2)
+plot(test, col = "blue", main = "width2") 
+plot(wtf, add = T, col ="red") 
+#mmk that did basically nothing
+
+test <- gBuffer(popnhrsoutline, width = 1000) #i assume width is m
+plot(test, col = "blue", main = "width2") 
+plot(wtf, add = T, col ="red") # still going to miss those points
+plot(spdf.sp, add = T)
+
+test <- gBuffer(popnhrsoutline, byid = TRUE, width = 10000) #byid makes spdf
+plot(test, col = "blue", main = "width2") 
+plot(wtf, add = T, col ="red") 
+plot(spdf.sp, add = T)
+# oh my god finally, 3 hours later i have the kde i want
+# now see whether to use kde or mcp
+
+plot(popnhrsallmcp, add = T, col = "blue")
+popnhrsallmcp@data$area # 8634958
+test@polygons[1] #86630053796
+plot(spdf.sp, add = T)
+plot(popnhrsmcp, add = T, col = "red")
+
+plot(test); plot(spdf.sp, add = T)
+# kde ftw
+
+# which sucks bc now we have to remove that pesky little hole in the middle...
+test@polygons # goal: index Slot "hole" from here (heh)
+test@polygons[[1,2]]
+test@polygons[1]
+test@polygons[1,]
+test@polygons[1,1]
+test@polygons[[1]]
+
+# help me stackoverflow, youre my only hope
+slotNames(test)
+#huh, probably should;ve known i could do that...
+slotNames(test@polygons)
+#newp
+slotNames(test@polygons[1])
+#newp
+slotNames(test@polygons[[1]])
+#my god kristin, you really need to get this double bracket thing down
+test@polygons[[1]]@Polygons
+test@polygons[[1]]@Polygons[[1]]
+#oooooh
+
+kdeoutline <- test@polygons[[1]]@Polygons[[1]]
+kdeoutline
+plot(kdeoutline)
+#fuckkkkk
+#ok i think this fails to pull actual spatial data with it (?)
+
+# indexing hole slot, finally :)
+test@polygons[[1]]@Polygons[[1]]@hole
+test@polygons[[1]]@Polygons[[2]]@hole
+
+# maybe write my own function for once? 
+# based on other people's work of course
+diehole <- function(x) {
+ if (x@hole == TRUE) print("nice work") else x 
+}
+
+diehole(test@polygons[[1]]@Polygons[[1]])
+diehole(test@polygons[[1]]@Polygons[[2]])
+#ok so this correctly identifies and extracts the polygon programmatically
+#but you need to drop the hole and keep allll the other data
+
+#ok i think youve sunk too much time into this sorry bro
+#just replace the inital polygons slot with only the outline one?
+
+plz <- test
+test@polygons <- test@polygons[[1]]@Polygons[[1]]
+#god dammit
+
+diehole <- function(x) {
+  if (x@hole == TRUE) NULL else x 
+}
+
+unholey <- function(x) {
+  noHoles <- lapply(x@polygons, diehole)
+  # Put the un holey Polygons list back into the @polygons slot 
+  x@polygons <- noHoles
+  #return the modified SpatialPolygonsDataFrame 
+  x
+}
+
+eh <- unholey(test)
+test@polygons
+
+diehole <- function(x) {
+  if (x@hole == TRUE) NULL else x 
+}
+noHoles <- lapply(test@polygons[[1]]@Polygons, diehole)
+test@polygons <- noHoles
+test
+plot(test)
+#KILL MEEEEEEEEEEEEEE
+
+# ok. you have made progress. almost there.
+# right now the hole is a NULL
+# you want it to FUCKING DIE
+# try going back to your original spolydf
+
+test <- gBuffer(popnhrsoutline, byid = TRUE, width = 10000) #byid makes spdf
+test[1]
+test[1,]
+test[[1]]
+test[[1,]]
+test[[1,2]]
+test[1,2]
+test@polygons[[1]]@Polygons
+test@polygons[1]
+test@polygons[2]
+names(test)
+  # these are only from @data
+
+slotNames(test)
+  # these are the slots
+
+slotNames(test@polygons)
+#NULL
+
+names(test@polygons)
+#also NULL
+
+meh <- SpatialPolygons(list(Polygons(list(test@polygons[[1]]@Polygons[[1]]),ID=1)))
+plot(meh)
+# jesus h christ that was all i needed?
+# granted this isn't a spoly dataframe, just a spoly...
+# see if that'll work
+writeOGR(z, 
+         dsn = "../GIS/Shapefiles/Elk", 
+         layer = "DidThisWork", 
+         driver = "ESRI Shapefile",
+         overwrite = TRUE)
+# motherfucker, nope, gotta be a spolydf
+a <- list(Polygons(list(test@polygons[[1]]@Polygons[[1]]),ID=1))
+test@data
+b <- test@data
+cmon <- SpatialPolygonsDataFrame(a, b)
+# error with your a
+
+# think kristin
+
+howbout <- test[test@polygons <- meh]
+
+
+# think harder
+
+str(test@polygons)
+test@polygons
+test@polygons[1]
+str(test@polygons[1])
+str(test@polygons[1]@Polygons)
+  # error here, can't pulla slot from a list
+test@polygons[1]@Polygons
+  # ditto above
+str(test@polygons[[1]])
+str(test@polygons[[1,1]]) # predict won't work bc now need to call slot
+# woo
+str(test@polygons[[1]]@Polygons)
+
+a <- list(Polygons(list(test@polygons[[1]]@Polygons[[1]]),ID=1))
+
+z <- test
+z@polygons[[1]]@Polygons <- a   
+plot(z)
+plot(test)
+
+b <- list(test@polygons[[1]]@Polygons[[1]])
+z@polygons[[1]]@Polygons <- b
+plot(z)
+# hallefuckingluyah
+
+
+## remove weird -attr data from coords of polygon
+
+test <- buff
+test@polygons[[1]]@Polygons[[1]]@coords
+str(test@polygons[[1]]@Polygons[[1]]@coords)
+test@polygons[[1]]@Polygons[[1]]@coords[1]
+test@polygons[[1]]@Polygons[[1]]@coords[[1]]
+test@polygons[[1]]@Polygons[[1]]@coords[1,2]
+
+# check whether this one works
+
+## export buffered area of all elk ranges all year
+writeOGR(obj = popnhrsoutline, 
+         verbose = TRUE,
+         dsn = "../GIS/Shapefiles/Elk", 
+         layer = "DoesThisWork", 
+         driver = "ESRI Shapefile")
+# yes,want str of buff to match this
+
+test@polygons[[1]]@Polygons[[1]]@coords
+str(test@polygons[[1]]@Polygons[[1]]@coords)
+test@polygons[[1]]@Polygons[[1]]@coords[1,]
+
+a <- matrix(as.numeric(test@polygons[[1]]@Polygons[[1]]@coords))
+test@polygons[[1]]@Polygons[[1]]@coords <- a
+a
+str(a)
+attr(test@polygons[[1]]@Polygons[[1]]@coords, "dimnames")
+attr(test@polygons[[1]]@Polygons[[1]]@coords, "dim")
+
+test@polygons[[1]]@Polygons[[1]]@coords
+
+test <- buff
+test@polygons[[1]]@Polygons[[1]]@coords
+test@polygons[[1]]@Polygons[[1]]@coords
+
+
+
+# attr() help
+x <- 1:10
+x
+attr(x, "dim") <- c(2,5)
+x
+attr(x, "dimnames") <- list(c("g", "f"))
+x
+str(x)
+# ok you now have an x with attributes including a null
+
+test <- buff
+attr(test@polygons[[1]]@Polygons[[1]]@coords, "dim")
+
+# task1: try to remove attr from x
+str(x)
+
+z <- unname(x)
+z
+str(z)
+
+test@polygons[[1]]@Polygons[[1]]@coords <- unname(test@polygons[[1]]@Polygons[[1]]@coords)
+
+#### alright, i fucking give
+# have to remove hole in arcmap like some kind of caveman
+
+outline <- list(buff@polygons[[1]]@Polygons[[1]])
+buff@polygons[[1]]@Polygons <- outline
+plot(buff)
+# remove NULL attribute data causing writeOGR to fail
+buff@polygons[[1]]@Polygons[[1]]@coords <- unname(buff@polygons[[1]]@Polygons[[1]]@coords)
+buff@polygons[[1]]@plotOrder <- as.integer(1)
+
+## fuck that wasn't the fucking problem fucking kill me
+
+# issue is with gBuffer because it fucks up when you have holes
+# so either export unbuffered thing into arcmap, remove and buffer there
+# or figure out here
+#first step: use adehabitat's buffer() instead
+# which requires a sppixdf (reason you didn't use before)
+
+z <- as(popnhrsoutline, "SpatialPixelsDataFrame")
+# can't convert spoly to spix
+
+test <- popnhrsoutline
+test@polygons[[1]]@Polygons[[2]]@hole
+
+for (i in 1:3) {
+ print(test@polygons[[1]]@Polygons[[i]]@hole)
+}
+
+for (i in 1:3) {
+ if (test@polygons[[1]]@Polygons[[i]]@hole == FALSE) print("yes") else print("no")
+}
+
+for (i in 1:3) {
+  if (test@polygons[[1]]@Polygons[[i]]@hole == TRUE) NULL else test@polygons[[1]]@Polygons[[i]]@hole
+}
+
+# lets try another tack
+test <- popnhrsoutline
+test <- slot(test, "polygons") <- lapply(slot(test, "polygons"),
+                                   checkPolygonsHoles)
+
+lapply(slot(test, "polygons"), comment)
+# this makes a "comment" of 0 if outline, 1 if hole, big whoop
+
+slot(test, "polygons") <- lapply(slot(test, "polygons"),
+                                   "comment<-", NULL)
+# removes comments
+
+## ok so take-home is i need to either use createSPComment() or modified code above
+# to create a comment about whether something is ahole or not
+# before I buffer popnhrsoutline.
+# great. perfect. can't wait to waste another few hours on this.
+
+
+
+## back at it, now with food etc for optimum brain function
+
+# Warning message:
+#   In gBuffer(popnhrsoutline, byid = TRUE, width = 10000) :
+#   Polygons object missing comment attribute ignoring hole(s). See function createSPComment.
+
+# i think i need to add an actual attribute that's a comment abt the hole
+?createSPComment
+  # yes, a comment for the Polygons class identifying polygon to which hole belongs
+
+# goal1: create a comment about holes for each polygon in original, non-buffered spolys
+test <- popnhrsoutline
+
+test@polygons[[1]]@Polygons # this is my list of Polygons to which I want to add comments
+str(test@polygons[[1]]@Polygons)
+
+# is there an easier way to get to this slot?
+?slot
+slot(test@polygons[[1]], "Polygons") #not super helpful
+slotPoly <- slot(test@polygons[[1]], "Polygons")
+  #ok this is a LIST of the 3 Polygons you want to add comments to
+  #so NOW you can start trying to use lapply on slotPoly
+
+# from bivand's rsig-geo(?) post
+lapply(slot(test@polygons[[1]], "Polygons"), checkPolygonsHoles)
+# why no work?
+# error says it's not a Polygons object
+str(slot(test@polygons[[1]], "Polygons"))
+# well. it's a LIST of Polygons objectS...
+
+pls1 <- lapply(slotPoly, function(p) {slot(p, "hole") <- FALSE; return(p)})
+pls1
+lapply(slot(createSPComment(usa1), "polygons"), comment)
+
+?gIsValid
+
+## never mind, youve sunk way too much time into this
+## admitting defeat and using arcmap
+## useless remaining code is:
+
+## add 1km buffer just in case, and remove hole
+buff <- gBuffer(popnhrsoutline, byid = TRUE, width = 10000) #byid makes spdf
+
+
+## export buffered area of all elk ranges all year
+writeOGR(buff,
+         verbose = TRUE,
+         dsn = "../GIS/Shapefiles/Elk", 
+         layer = "AllElkYrHR", 
+         driver = "ESRI Shapefile",
+         overwrite_layer = TRUE)
