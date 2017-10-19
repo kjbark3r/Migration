806750c6a26089c1d77f25d2b14392f8f57ce515
MCWRU01-LW17\kristin <kristin.barker@umconnect.umt.edu>
2017-10-18 21:01:31 -0600

    Cropped and stored NDVI data relevant to each population and time period of interest (for later covariate calculations)

--- rasterclip.R
+++ rasterclip.R
@@ -17,12 +17,12 @@
 
     library(raster) # ...rasters...
     library(sp) # spatial
-    library(rgeos) # buffer
-    library(adehabitatHR) # home ranges and kernel centroids
+#    library(rgeos) # buffer
+#    library(adehabitatHR) # home ranges and kernel centroids
     library(rgdal) # projections; working with shps
-    library(gsubfn) # no idea, possibly unnecessary
+#    library(gsubfn) # no idea, possibly unnecessary
     library(maptools) # writeSpatialShape
-    library(ggmap) # in case you want to plot in R
+#    library(ggmap) # in case you want to plot in R
     library(dplyr) # joins, data work, general awesomeness
 
 
@@ -39,7 +39,7 @@
         setwd(wd_worklaptop)
       }
     }
-
+    rm(wd_workcomp, wd_laptop, wd_worklaptop)
 
 #### Projections ####
     
@@ -54,18 +54,15 @@
     popnyr <- read.csv("popns-yrs.csv")
 
     
-    # list years of interest
-    yrs <- list(unique(popnyr$Year))
-    
     # full area of interest (from homeranges.R and post-hoc ArcMap buffering)
     aoiraw <- readOGR("../GIS/Shapefiles/Elk", layer = 'AreaOfInterest')
     
     
     # growing season home range for each population (from homeranges.R)
-    popnhrs <- readOGR("../GIS/Shapefiles/Elk", layer = 'PpnGrowingSsnHRs')
+    popnhrs <- readOGR("../GIS/Shapefiles/Elk/PopnHRs", layer = 'PpnGrowingSsnHRs')
     
     
-    # NDVI amplitude
+    # NDVI amplitude (from https://phenology.cr.usgs.gov/get_data_250w.php)
     files.amp <- list.files(
         path = "./NDVIamp/",
         pattern = "tif$",
@@ -74,7 +71,7 @@
     names(ampstk)
     ampstk@crs
     
-    # time-integrated NDVI
+    # time-integrated NDVI (from https://phenology.cr.usgs.gov/get_data_250w.php)
     files.ti <- list.files(
       path = "./NDVIti/",
       pattern = "tif$",
@@ -83,18 +80,10 @@
     names(tistk)
     tistk@crs
 
-
-### ### ### ### ### ### ### ### ##
-####  |REMOTELY SENSED DATA|  ####
-### ### ### ### ### ### ### ### ##
-
     
-    # clip to area of interest 
-    aoi <- spTransform(aoiraw, crs(ampstk)) # match projections
-    ampcrop <- crop(ampstk, aoi) # crop (note: makes stacks to bricks
-    ticrop <- crop(tistk, aoi)   #       which speeds processing)
+
+#### Tweaked data  ####
     
-    # create lists and info for loop(?) prep
     
     # population codes and year indices
     popdat <- popnyr %>%
@@ -117,21 +106,43 @@
                    ifelse(Herd == "Tobacco Roots", "tob",
                    ifelse(Herd == "West Fork", "wfk", 
                           NA)))))))))))))))))
-
+    
     # verify home ranges are in same order as popdat
     popdat$Herd
     popnhrs@data$id
-    
 
     
     
+### ### ### ### ### ### ### ### ##
+####  |REMOTELY SENSED DATA|  ####
+### ### ### ### ### ### ### ### ##
+
+    
+    # crop all ndvi data to area of interest (to speed processing)
+    aoi <- spTransform(aoiraw, crs(ampstk)) # match aoi proj to ndvi
+    ampcrop <- crop(ampstk, aoi) # crop (note: makes stacks to bricks
+    ticrop <- crop(tistk, aoi)   ##       which speeds processing)
+    ampcropsp <- projectRaster(ampcrop, crs = crs(popnhrs)) # make ndvi
+    ticropsp <- projectRaster(ticrop, crs = crs(popnhrs)) ## stateplane
+
+
+    # remove unnecessary items from workspace to save some memory
+    rm(ampcrop, ticrop, aoiraw, files.amp, files.ti)
+    
+    
+    # set output paths
+    ampout <- "./NDVIamp/processed/"
+    tiout <- "./NDVIti/processed/"    
+    
+    
+
     for(i in 1:nrow(popdat)) {
       
       # for each population
       pop <- popdat$Pop[i]
       
       # identify growing season home range polygon
-      hr <- popnhrs@polygons[[i]]@Polygons[[1]]
+      hr <- popnhrs[popnhrs$id[i],]
       
       # identify year of interest
       yr <- popdat$yrIndex[i]
@@ -140,37 +151,53 @@
       yrs <- c(yr-5, yr-4, yr-3, yr-2, yr-1, yr)
       
       # identify NDVIamp and tiNDVI layers for each of those years
-      amp <- subset(ampcrop, yrs)
-      ti <- subset(ticrop, yrs)
+      ampsub <- subset(ampcropsp, yrs)
+      tisub <- subset(ticropsp, yrs)
       
-      # crop layers to population growing season range
-      ampc <- crop(amp, hr)
-      tic <- crop(ti, hr)
-      # CURRENT ISSUE: YOUR SUBSETTED HR POLYGON IS JUST A POLYGON
-      # NOT A SPATIAL POLYGON
-      # SO THIS CROPPING STEP CAN'T WORK
+      # create empty rasterstacks to store results in
+      ampstk <- stack()
+      tistk <- stack()
       
-      # store cropped layers as bricks
-      #brick(x = , filename = paste0())
       
-    }
+      for (j in 1:length(yrs)) {
+       
+        # for each relevant year
+        ampuncut <- ampsub[[j]]
+        tiuncut <- tisub[[j]]
+        
+        # identify ndvi cells that are within or touching edge of home range
+        cells <- cellFromPolygon(ampuncut, hr, weights = TRUE)[[1]][, "cell"]
+        
+        # set all other cells to NA
+        ampuncut[][-cells] <- NA
+        tiuncut[][-cells] <- NA
+        
+        # remove NA cells
+        amp <- trim(ampuncut)
+        ti <- trim(tiuncut)
+        
+        # combine results in stack
+        ampstk <- stack(ampstk, amp)
+        tistk <- stack(tistk, ti)
+
+      }
+      
+      
+      # store growing season HR, NDVIamp, and tiNDVI relevant to each popn
+      
+      writeOGR(hr, 
+               dsn = "../GIS/Shapefiles/Elk/PopnHRs",
+               layer = paste0(pop, "-HRgrow", yr+2000),
+               driver = "ESRI Shapefile", overwrite = TRUE)
+      
+      writeRaster(ampstk,
+                  filename = paste0(ampout, pop, "-", names(ampstk)),
+                  bylayer = TRUE, format = "GTiff", overwrite = TRUE)
+      
+      writeRaster(tistk,
+                  filename = paste0(tiout, pop, "-", names(tistk)),
+                  bylayer = TRUE, format = "GTiff", overwrite = TRUE)
     
-    
-    
-    
-    
-#### old code ####
-
-
-
-# # crop one raster to another
-# kud99 <- getverticeshr(kud.all, percent = 99) # gives outline of home range
-# lc.crop <- crop(lc, kud99) # lc is a landcover raster – needs to be same projection
-# library(raster) # mask is in this library
-# lc.crop <- mask(lc, kud99) # magically gets rid of NAs or something
-# plot(lc.crop)
-
-
-#### revisit nearest neighbor vs bilinear ####
-
-# ndvi - NEED EVERY RASTER FROM EVERY TIME PD IN EVERY YR
+  }
+   
+ 
