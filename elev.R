c386ce1bb91110531f0159d839469f6b78055242
MCWRU01-LW17\kristin <kristin.barker@umconnect.umt.edu>
2018-03-25 11:30:44 -0600

    extract min & max elevs across study area for statewide ms

--- elev.R
+++ elev.R
@@ -1,6 +1,7 @@
 ### ### ### ### ### ### ### ### ### ### ### ### ##
 ###  EXTRACT ELEVATIONS FOR ALL ELK LOCATIONS  ###
 ### 	  	 Kristin Barker  Nov 2017	      	   ###
+###   OH AND ACROSS THE STUDY AREA Mar 2018    ###
 ### ### ### ### ### ### ### ### ### ### ### ### ##
 
 
@@ -48,3 +49,23 @@ locelevs <- cbind(locs, elevs) %>%
   within(Sex <- "Female") %>%
   dplyr::select(AnimalID, Herd, DateTime, Latitude, Longitude, Sex, Elev)
 write.csv(locelevs, "../locs-allcows-withelevs.csv", row.names = FALSE)
+
+
+
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### 
+
+### ###  ### ### ###
+#### STUDY AREA ####
+### ###  ### ### ###
+
+library(beepr)
+memory.limit(size = 7500000)
+#### read in that raster i already conveniently created
+ras <- raster("alldems.tif")
+#plot(ras)
+aoi <- shapefile("../GIS/Shapefiles/Elk/AreaOfInterest.shp")
+aoi <- spTransform(aoi, ras@crs)
+#plot(aoi, add = TRUE)
+aoielevs <- raster::intersect(ras, aoi)
+writeRaster(aoielevs, "elevsAoi", format = "GTiff")
+min(aoielevs) # this isn't the right fcn probably but it works to display min/max
