3f3cc4604c6bfa68386dfd95b7c1b882d56d11f2
MCWRU01-LW17\kristin <kristin.barker@umconnect.umt.edu>
2018-03-27 18:57:31 -0600

    first run at worldclim data, not there yet

--- elev.R
+++ elev.R
@@ -58,14 +58,70 @@ write.csv(locelevs, "../locs-allcows-withelevs.csv", row.names = FALSE)
 #### STUDY AREA ####
 ### ###  ### ### ###
 
-library(beepr)
-memory.limit(size = 7500000)
-#### read in that raster i already conveniently created
-ras <- raster("alldems.tif")
-#plot(ras)
-aoi <- shapefile("../GIS/Shapefiles/Elk/AreaOfInterest.shp")
-aoi <- spTransform(aoi, ras@crs)
-#plot(aoi, add = TRUE)
-aoielevs <- raster::intersect(ras, aoi)
-writeRaster(aoielevs, "elevsAoi", format = "GTiff")
-min(aoielevs) # this isn't the right fcn probably but it works to display min/max
+
+  # goal: determine min and max elev, temp, and precip across western MT study area #
+
+
+  #### misc prelim ####
+
+    memory.limit(size = 7500000) # jic
+    library(rgdal)
+    library(raster)
+    
+
+  #### data ####
+  
+    # read in data
+    aoi <- shapefile("../GIS/Shapefiles/Elk/AreaOfInterest.shp") # study area
+    tmin <- getData('worldclim', var = 'tmin', res = 10) # min temp
+    tmax <- getData('worldclim', var = 'tmax', res = 10) # max temp
+    precip <- getData('worldclim', var = 'prec', res = 10) # precip
+    ras <- raster("alldems.tif") # elevs (i created this previously)
+    
+    # note different projections...
+    crs(aoi); crs(ras); crs(tmin); crs(precip)
+
+    # list of the data
+    allDat <- c("tmin", "tmax", "precip", "ras")
+    str(allDat)
+    allDat[1]
+      
+
+  #### extract ####
+ 
+    pullClim <- function(x, y) { # function to clip data (x) to study area (y) 
+     y <- spTransform(y, x@crs) # match study area projection to data       
+     inAoi <- raster::mask(x, aoi) # extract only data within study area
+     summary(inAoi) # display min/max [DOES NOT STORE]
+    }
+    
+    
+    # loop through each data source, clip and display min/max
+    for (i in 1:length(allDat)) {
+      sArea <- aoi
+      cDat <- get(allDat[i])
+      cCrop <- pullClim(cDat, sArea)
+    }
+    
+    ## errored out on last iteration
+    ## and you weren't clever enough to store outputs of each, du
+    ## also you need to pull data from the right years...
+    
+    
+    
+    # can delete
+    test <- tmin[[1]]
+    test <- raster::mask(test, aoi)
+    plot(test)
+    plot(aoi, add = T)
+    summary(test)
+    
+    
+    crs(tmin)
+    plot(tmin)
+    ?getData
+    
+    
+    
+    
+    
